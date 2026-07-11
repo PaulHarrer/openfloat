393408bf12f4da7bb1d7d3806c901a5f9c9bb475
Paul Harrer <16596572+PaulHarrer@users.noreply.github.com>
2026-07-11 21:21:57 +0200

    Patch: bumped version

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     name: 'OpenFloat',
     description: "Adds PiP (Picture-in-Picture) toggle to sites that don't support it natively.",
-    version: '1.1.0',
+    version: '1.2.0',
     author: 'Paul Harrer',
     icons: {
       16: 'icons/icon16.png',
